7137b76896fe80a227f708fdc106948b5eb816aa
Juan Silva <juanssl@gmail.com>
2015-12-13 22:28:40 -0800

    Added Helper to convert data

---
+++ JSONHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace MiniJSON
+{
+	public static class JSONHelper
+	{
+		#region Numbers
+		public static short ConvertToShort(object obj)
+		{
+			if (obj is long)
+				return (short)((long)obj);
+
+			if(obj is double)
+				return (short)((double)obj);
+
+			return 0;
+		}
+
+		public static int ConvertToInt(object obj)
+		{
+			if (obj is long)
+				return (int)((long)obj);
+
+			if(obj is double)
+				return (int)((double)obj);
+			
+			return 0;
+		}
+
+		public static long ConvertToLong(object obj)
+		{
+			if (obj is long)
+				return (long)obj;
+
+			if(obj is double)
+				return (long)((double)obj);
+
+			return 0;
+		}
+
+		public static float ConvertToFloat(object obj)
+		{
+			if (obj is double)
+				return (float)((double)obj);
+
+			return 0;
+		}
+		#endregion
+
+		#region String
+		public static string ConvertToString(object obj)
+		{
+			if (obj is string)
+				return (string)obj;
+
+			return null;
+		}
+		#endregion
+
+		#region Boolean
+		public static bool ConvertToBool(object obj)
+		{
+			if (obj is bool)
+				return (bool)obj;
+
+			return false;
+		}
+		#endregion
+	}
+}
+
